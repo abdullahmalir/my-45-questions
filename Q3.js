5bebe6e1b44058dadb36fa97d7359fc2dfba2e5b
abdullahmalir <164551005+abdullahmalir@users.noreply.github.com>
2024-03-22 22:11:46 -0700

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,11 @@
+var personName = "aqsa";
+console.log(personName);
+//lower case
+console.log(personName.toLowerCase());
+// uppercase
+console.log(personName.toUpperCase());
+//tital case
+console.log(personName.charAt(0));
+console.log(personName.charAt(0).toUpperCase());
+console.log(personName.slice(1, 4));
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1, 4));
